158ff502ae03163466977179c4c28535faf8ba17
mhrayhan <mhrayhan430@gmial.com>
2022-04-19 21:06:36 +0530

    about me done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import './About.css'
 import imgMe from '../../images/me2.png'
+import './About.css'
 
 const About = () => {
     return (
@@ -9,8 +10,8 @@ const About = () => {
             <Header></Header>
                 <h2 className='mt-5 text-center'>About Me</h2>
             <div className=' mt-3 d-flex align-items-center justify-content-center'>
-                <div className='aobut-me w-50'>
-                    <h3>I'm Mozammel Hoque</h3>
+                <div className='about-me w-50'>
+                    <h1>I'm Mozammel Hoque</h1>
                     <p>I'm a proud student of 'Programming Hero'. My goal is to see myself as a Frontend Developer in 2022. I learnet a lot from my favorit teacher 'Jhankar Mahbub'. He motivated me to start my career as a Developer.</p>
                 </div>
                 <img className='rounded mt-3' src={imgMe} alt="" />
